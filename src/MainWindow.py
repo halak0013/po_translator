0b3c285c8570001bcc0e00c53c7a57bc9e880189
Muhammet <zumruduanka0013@gmail.com>
2023-08-07 14:53:00 +0300

    ui improvement for pot update

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -54,6 +54,7 @@ class MainWindow(Gtk.Window):
 
     def init_variables(self):
         self.pot_file_path=""
+        self.po_update_files=""
         self.selected_lang_codes=[]
         self.translator=Po_Translator2()
 
@@ -102,7 +103,9 @@ class MainWindow(Gtk.Window):
         self.chck_lng_pt: Gtk.CheckButton = self.builder.get_object("chck_lng_pt")
 
         self.file_po: Gtk.FileChooserButton = self.builder.get_object("file_po")
+        self.file_po_update_files: Gtk.FileChooserButton = self.builder.get_object("file_po_update_files")
         self.btn_translate: Gtk.Button = self.builder.get_object("btn_translate")
+        self.chck_po_up_file: Gtk.CheckButton = self.builder.get_object("chck_po_up_file")
 
     def fill_lang_codes(self):
         for i,l in enumerate(self.chck_lang_list):
@@ -116,18 +119,34 @@ class MainWindow(Gtk.Window):
     def on_chck_lng_toggled(self,b):
         pass
 
+    def on_chck_po_up_file_toggled(self, b):
+        self.file_po_update_files.set_sensitive(b.get_active())
+
     def on_file_po_file_set(self,f):
         self.pot_file_path=self.file_po.get_filename()
         print(self.file_po.get_filename())
 
+    def on_file_po_update_files_file_set(self,f):
+        self.po_update_files=self.file_po_update_files.get_filename()
+        print(self.file_po.get_filename())
+
     def on_btn_translate_clicked(self,b):
-        if os.path.exists(self.pot_file_path):
+        if not os.path.exists(self.pot_file_path):
+            print("pot file not found, please try again")
+        elif self.chck_po_up_file.get_active():
+            if not os.path.exists(self.po_update_files):
+                print("files file not found, please try again")
+            else:
+                self.fill_lang_codes()
+                if not os.path.exists("/tmp/translate/"):
+                    os.makedirs("/tmp/translate")
+                self.translator.start_translation(self.pot_file_path,self.selected_lang_codes,
+                                                  self.po_update_files,self.chck_po_up_file.get_active())
+        else:
             self.fill_lang_codes()
             if not os.path.exists("/tmp/translate/"):
                 os.makedirs("/tmp/translate")
             self.translator.start_translation(self.pot_file_path,self.selected_lang_codes)
-        else:
-            print("pot file not found, please try again")
 
 
     def destroy(self):
