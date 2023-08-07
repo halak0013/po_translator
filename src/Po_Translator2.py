8ba1ebe902e73bb445ded7727feea68b9cd20917
Muhammet <zumruduanka0013@gmail.com>
2023-08-07 13:13:34 +0300

    now if there exit po file it update empty space

--- src/Po_Translator2.py
+++ src/Po_Translator2.py
@@ -1,3 +1,4 @@
+import os
 import polib
 import threading
 import requests
@@ -8,12 +9,15 @@ class Po_Translator2:
         pass
 
     def get_po_file(self,file):
+        path=self.get_path(file)
+        
         po = polib.pofile(file)
-        return [e.msgid for e in po]
+        return [e.msgid for e in po if e.msgstr == ""]
 
     def get_translation_api(self,text, lang_code):
         text = quote(text)
         url=f"https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl={lang_code}&dt=t&q={text}"
+        result=""
         try:
             with requests.get(url, stream=True) as response:
                 response.raise_for_status()
@@ -22,8 +26,12 @@ class Po_Translator2:
                         file.write(chunk)
             with open("/tmp/translate/t.txt", "r") as f:
                 tr = f.readline()
-                translation=json.loads(tr)[0][0][0]
-                return translation
+                print(tr)
+                for d in json.loads(tr)[0]:
+                    while not type(d) == str:
+                        d = d[0]
+                    result += d
+            return result
         except requests.exceptions.RequestException as e:
             print(f"File download error: {e}")
         return "null"
@@ -41,11 +49,14 @@ class Po_Translator2:
         # Generate the new PO file with the translations
         self.generate_po_file(pot_file, translations,lang_code)
 
-    def generate_po_file(self,pot_file_path, translations,lang_code):
+    def get_path(self, pot_file_path):
         last_slash_index = pot_file_path.rfind('/')
         # Extract the path before the last '/'
         path = pot_file_path[:last_slash_index + 1]
+        return path
 
+    def generate_po_file(self,pot_file_path, translations,lang_code):
+        path=self.get_path(pot_file_path)
         po = polib.pofile(pot_file_path)
         for entry in po:
             msgid = entry.msgid
@@ -54,9 +65,17 @@ class Po_Translator2:
         po.save(f"{path}{lang_code}.po")
         print(f"{path}{lang_code}.po saved")
 
+    def is_po_file_exists(self,lang_code):
+        po_file=f"{self.get_path(self.pot_file_path)}{lang_code}.po"
+        if os.path.exists(po_file):
+            return po_file
+        else:
+            return self.pot_file_path
+
     def generate_translation(self):
         for l in self.lang_codes:
-            self.translate_and_generate_po(self.pot_file_path,l)
+            self.translate_and_generate_po(self.is_po_file_exists(l),l)
+        print("finished")
 
     def start_translation(self,pot_file_path,lang_codes):
         self.pot_file_path=pot_file_path
